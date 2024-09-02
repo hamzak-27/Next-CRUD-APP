a1da46513b16360ba97d8b72b0df65b9d9f250f2
Hamza Khan <maestrohk27@gmail.com>
2024-09-02 07:52:55 +0530

    Add files via upload

---
+++ libs/mongodb.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const connectMongoDB = async () => {
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log('Connected to MongoDB.');
+    }
+    catch(error){
+
+        console.log(error);
+
+    }
+
+}
+
+export default connectMongoDB;
